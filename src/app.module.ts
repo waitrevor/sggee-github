392d25f88bdd541e95736ba903bb23c448bc1a8a
Trevor <trevorwai2000@gmail.com>
2024-12-06 01:12:37 -0700

    Updates

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { GithubModule } from './github/github.module';
+import { GithubModule } from './modules/github.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, GithubModule, ConfigModule.forRoot()],
+  imports: [GithubModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
